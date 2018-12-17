c8baa244239cce77fc7a92d019573a8b16e83515
sevastiansergiu <->
2018-12-17 20:56:40 +0200

    Sign-up Customer Page

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -27659,7 +27659,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57461" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57922" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
