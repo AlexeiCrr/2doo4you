7d5941672ba7034cb4615a4b9ac832d07562eb55
Alex Crudu <crudu-alexei@mail.ru>
2018-12-17 18:06:10 +0200

    finished styling the front page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import '../js/main';
 import $ from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+import fs from 'fs';
 import fontawesome from '@fortawesome/fontawesome'
 import regular from '@fortawesome/fontawesome-free-regular'
 import solid from '@fortawesome/fontawesome-free-solid'
