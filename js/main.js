469d508ce962713c2b6fb5c44b14d50966d1a140
Alex Crudu <crudu-alexei@mail.ru>
2018-12-20 01:27:08 +0200

    styled home page for responsive, styled the header and added custom mobile menu

--- js/main.js
+++ js/main.js
@@ -1,39 +1,98 @@
 import $ from 'jquery';
+import slick from 'slick-carousel';
+import BadgerAccordion from "badger-accordion";
+
+if ($('body').hasClass('home-page')) {
+    $('.carousel--multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // mobileFirst: true,
+        arrow: true,
+        // respondTo: 'window',
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 2,
+                    // arrow: false,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+}
 
 $(document).ready(function () {
-    if ($('body').hasClass('home-page')) {
-        $('.carousel--multiple-items').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            arrow: true,
+    //Home Page Carousel
+    $('.form-row__actions a').on('click', function (e) {
+        e.preventDefault();
+        $('.form-row__actions a').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Color stars
+    function stars() {
+        var star_tap = $('.stars-section .fa-star');
+        star_tap.on('click', function () {
+            console.log('click');
+            $(this).removeClass('color_star').removeClass('current');
+            $(this).removeClass('fas');
+            $(this).addClass('far');
+
+            var index = $(this).index();
+            star_tap.each(function (i) {
+                if ($(this).index() <= index) {
+                    $(this).removeClass('far');
+                    $(this).addClass('fas');
+                    $(this).addClass('color_star');
+                }
+            });
         });
     }
-    $('.form-row__actions a').on('click', function (e) {
-      e.preventDefault();
-      $('.form-row__actions a').removeClass('active');
-      $(this).addClass('active');
-    });
 
-  // Color stars
-  function stars() {
-    var star_tap = $('.stars-section .fa-star');
-    star_tap.on('click', function () {
-      console.log('click');
-      $(this).removeClass('color_star').removeClass('current');
-      $(this).removeClass('fas');
-      $(this).addClass('far');
-
-      var index = $(this).index();
-      star_tap.each(function (i) {
-        if ($(this).index() <= index) {
-          $(this).removeClass('far');
-          $(this).addClass('fas');
-          $(this).addClass('color_star');
-        }
-      });
-    });
-  }
-
-  stars();
+    stars();
 });
+
+//Accordion
+const accordions = document.querySelectorAll('.js-badger-accordion');
+
+Array.from(accordions).forEach((accordion) => {
+    const ba = new BadgerAccordion(accordion);
+});
+//See All/See less
+$(document).ready(function () {
+    $(".expand-selection").click(function () {
+        $(this).html(($(this).text() == "See Less") ? "See All" : "See Less");
+    });
+});
+//Mobile Menu
+$('#mobile-menu').click(toggleMenu);
+$('.second-level__trigger').click(toggleSecondLevelMenu);
+
+function toggleMenu() {
+    $(this).toggleClass("active-menu");
+    $('.sidebar').toggleClass('active-sidebar');
+}
+
+function toggleSecondLevelMenu() {
+    $(this).toggleClass('menu-open');
+    $('.second-level__content').toggleClass('active-second-level');
+}
+
+// var docWidth = document.documentElement.offsetWidth;
+//
+// [].forEach.call(
+//     document.querySelectorAll('*'),
+//     function (el) {
+//         if (el.offsetWidth > docWidth) {
+//             console.log(el);
+//         }
+//     }
+// );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import '../scss/main.scss'
+import '../scss/main.scss';
+import $ from 'jquery';
 import '../js/main';
-import $ from "jquery";
+import slick from 'slick-carousel';
 import bootstrap from "bootstrap";
 import popper from "popper.js";
 import fs from 'fs';
-import fontawesome from '@fortawesome/fontawesome'
-import regular from '@fortawesome/fontawesome-free-regular'
-import solid from '@fortawesome/fontawesome-free-solid'
+import fontawesome from '@fortawesome/fontawesome';
+import regular from '@fortawesome/fontawesome-free-regular';
+import solid from '@fortawesome/fontawesome-free-solid';
+
 
 fontawesome.library.add(regular);
 fontawesome.library.add(solid);
